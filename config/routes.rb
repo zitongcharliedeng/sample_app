013ad988461f75fbd8a2573d2a6186f7de3ad40b
charlie <charlie@example.com>
2022-08-04 20:14:30 +0100

    finished chap 3.5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root "application#hello"
   get 'foo/bar'
   get 'foo/baz'
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
 end
